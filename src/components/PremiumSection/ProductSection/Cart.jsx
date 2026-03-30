93ac05d4b17dac597610a87755cad538446da035
mesbahulislam1 <mesbahulislam012@gmail.com>
2026-03-30 19:16:23 +0600

    update

--- src/components/PremiumSection/ProductSection/Cart.jsx
+++ src/components/PremiumSection/ProductSection/Cart.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import CartIcon from '../../../assets/cartImg.jpeg'
 const Cart = ({ card, setCard }) => {
   const totalPrice = card.reduce((total, item) => total + item.price, 0);
   
@@ -12,7 +12,12 @@ const Cart = ({ card, setCard }) => {
     <div className="p-7 border border-black/18 rounded-2xl">
       <h2 className="text-2xl font-extrabold text-[#101727]">Your Cart</h2>
 
-      <div className=" space-y-2">
+      {
+        card.length === 0? <div>
+          <img src={CartIcon} alt="" className="w-[200px] mx-auto"/>
+          <h2 className="text-2xl text-center font-bold text-black/70">Your cart is empty</h2>
+        </div>: <div>
+          <div className=" space-y-2">
         {
           card.map((list, index)=> {
             return <div key={index} className=" bg-[#e0e0e0] p-5 flex items-center justify-between rounded-2xl">
@@ -40,6 +45,9 @@ const Cart = ({ card, setCard }) => {
         </div>
         <button className='bg-linear-to-l to-[#4F39F6] w-full from-[#9514FA] px-5 py-2.5 text-white cursor-pointer rounded-full'>Proceed to Checkout</button>
       </div>
+        </div>
+      }
+      
     </div>
   );
 };
